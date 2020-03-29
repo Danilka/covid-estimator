557fe4aee3ef15b4af16a1960cbb62e883ac088c
Danil Kozyatnikov <dan@danilink.com>
2020-03-29 01:31:33 -0700

    Automatic graph images & NYT data.

--- generate.py
+++ generate.py
@@ -5,6 +5,8 @@ import os
 from datetime import datetime
 from typing import Optional
 
+import matplotlib
+import matplotlib.pyplot as plt
 from jinja2 import FileSystemLoader, Environment
 import pandas as pd
 import numpy as np
@@ -18,14 +20,27 @@ __email__ = 'covidestimator@danilink.com'
 
 DEBUG = False
 SITE_URL = 'https://coronaviruspredictor.com/'
+# SITE_URL = 'http://private/var/www/covid-estimator/html/'
 SITE_BASE_PATH = '/var/www/covid-estimator/html/'
-DATA_BASE_PATH = '/var/www/COVID-19/'
+JH_DATA_BASE_PATH = '/var/www/COVID-19/'
+NYT_DATA_BASE_PATH = '/var/www/covid-19-data/'
 
 
-def date_to_dofy(date):
-    """Convert a date string into a day of the year int."""
+def jh_date_to_dofy(date):
+    """Convert a JH date string into a day of the year int."""
     return datetime.strptime(date, '%x').timetuple().tm_yday
 
+def dofy_to_jh_date(dofy):
+    """Convert day of the year into JH date."""
+    return datetime.strptime('2020 {}'.format(dofy), '%Y %j').strftime('%x')
+
+def nyt_date_to_dofy(date):
+    """Convert a NYT date string into a day of the year int."""
+    return datetime.strptime(date, '%Y-%m-%d').timetuple().tm_yday
+
+def nyt_date_to_jh_date(date):
+    """Convert a NYT date string into a JH date string."""
+    return datetime.strptime(date, '%Y-%m-%d').strftime('%x')
 
 def date_to_readable(date):
     """Convert a date string into a readable format."""
@@ -67,6 +82,20 @@ class DataEstimate:
         self.peak_day = peak_day
 
     @property
+    def estimate_order(self):
+        """Returns quantifiable order of the estimation."""
+        estimation_map = {
+            'good': 0,
+            'ok': 1,
+            'bad': 2,
+            'horrible': 3,
+        }
+        try:
+            return estimation_map[self.estimate]
+        except KeyError:
+            return None
+
+    @property
     def name(self):
         return '{}-{}'.format(self.country, self.province) if self.province else self.country
 
@@ -77,6 +106,14 @@ class DataEstimate:
     def get_url(self):
         return '{}{}'.format(SITE_URL, self.page_name)
 
+    @property
+    def current_cases_estimated(self):
+        return int(self.cases_approximation.tail(1))
+
+    @property
+    def current_cases(self):
+        return int(self.cases.tail(1))
+
     def to_chart_js(self):
         first_reported_case_index = self.cases.to_numpy().nonzero()[0][0]
         return {
@@ -84,8 +121,8 @@ class DataEstimate:
             'province': self.province,
             'country': self.country,
             'url': self.get_url(),
-            'current_cases_estimated': int(self.cases_approximation.tail(1)),
-            'current_cases': int(self.cases.tail(1)),
+            'current_cases_estimated': self.current_cases_estimated,
+            'current_cases': self.current_cases,
             'last_day_reported': date_to_readable(self.cases.tail(1).index[0]),
             'cases_a': str(list(self.cases_approximation.iloc[first_reported_case_index:])),
             'cases': str(list(self.cases.iloc[first_reported_case_index:])),
@@ -109,18 +146,24 @@ class CovidDataAnalyzer:
 
     df_cases: Optional[pd.DataFrame] = None
     df_deaths: Optional[pd.DataFrame] = None
+    states_population: Optional[pd.DataFrame] = None
 
-    def __init__(self, base_path='./', mortality_rate=3.4, avg_days_symptoms_to_death=14, avg_days_to_symptoms=5):
+    def __init__(self, jh_base_path='./', nyt_base_path='./', mortality_rate=3.4, avg_days_symptoms_to_death=14, avg_days_to_symptoms=5):
         """
-        :param base_path: Path to the Johns Hopkins' data repo https://github.com/CSSEGISandData/COVID-19
+        :param jh_base_path: Path to the Johns Hopkins' data repo https://github.com/CSSEGISandData/COVID-19
         :param mortality_rate: Median mortality rate in %. https://www.who.int/dg/speeches/detail/who-director-general-s-opening-remarks-at-the-media-briefing-on-covid-19---3-march-2020
         :param avg_days_symptoms_to_death: Median days from symptoms to death. https://www.worldometers.info/coronavirus/coronavirus-death-rate/#days
         :param avg_days_to_symptoms: Median days from infected to symptoms. https://www.ncbi.nlm.nih.gov/pubmed/32150748
         """
-        self.base_path = base_path
+        self.jh_base_path = jh_base_path
+        self.nyt_base_path = nyt_base_path
         self.mortality_rate = mortality_rate
         self.avg_days_symptoms_to_death = avg_days_symptoms_to_death
         self.avg_days_to_death = avg_days_to_symptoms + avg_days_symptoms_to_death
+
+        # Load states population data.
+        self.states_population = pd.read_csv('statepop.tsv', delimiter='\t')
+
 
     @staticmethod
     def logistic_model(day, infection_speed, maximum, total):
@@ -134,7 +177,7 @@ class CovidDataAnalyzer:
         """
         return total/(1+np.exp(-(day-maximum)/infection_speed))
 
-    def load_data(self, deaths_file_path, cases_file_path):
+    def load_data_jh(self, deaths_file_path, cases_file_path):
         """
         Load data from Johns Hopkins' data repo into the memory.
         :return: self
@@ -143,7 +186,7 @@ class CovidDataAnalyzer:
         # Load Confirmed Cases
         self.df_cases = pd.read_csv(
             os.path.join(
-                self.base_path,
+                self.jh_base_path,
                 cases_file_path
             )
         )
@@ -151,11 +194,69 @@ class CovidDataAnalyzer:
         # Load deaths
         df = pd.read_csv(
             os.path.join(
-                self.base_path,
+                self.jh_base_path,
                 deaths_file_path
             )
         )
         self.df_deaths = df.fillna(0)  # Fill nan with 0
+
+        return self
+
+    def load_data_nyt(self, states_file_path):
+        """
+        Load data from Johns Hopkins' data repo into the memory.
+        :return: self
+        """
+
+        # Load raw states data.
+        nyt_states_data = pd.read_csv(
+            os.path.join(
+                self.nyt_base_path,
+                states_file_path
+            )
+        )
+
+        last_data_day = nyt_date_to_dofy(nyt_states_data.tail(1)['date'].iloc[0])
+
+        df_cases = pd.DataFrame(
+            columns=['Province/State', 'Country/Region', 'Lat', 'Long']+[dofy_to_jh_date(x) for x in range(1, last_data_day)]
+        )
+
+        df_deaths = df_cases.copy()
+
+        for i, row in nyt_states_data.iterrows():
+            date_jh_format = nyt_date_to_jh_date(row['date'])
+            state = str(row['state'])
+            cases = int(row['cases'])
+            deaths = int(row['deaths'])
+
+            # just to make sure that we have the column with this date.
+            # In case the initial data is not properly ordered.
+            if date_jh_format not in df_cases.columns:
+                df_cases[date_jh_format] = 0
+                df_deaths[date_jh_format] = 0
+
+            # Alter the existing row or make a new entry for this state.
+            if len(df_cases.loc[(df_cases['Province/State'] == state) & (df_cases['Country/Region'] == 'US'), date_jh_format]):
+                df_cases.loc[(df_cases['Province/State'] == state) & (df_cases['Country/Region'] == 'US'), date_jh_format] = cases
+            else:
+                df_cases = df_cases.append(
+                    pd.Series([state, 'US', cases], index=['Province/State', 'Country/Region', date_jh_format]),
+                    ignore_index=True,
+                )
+
+            # Alter the existing row or make a new entry for this state.
+            if len(df_deaths.loc[(df_deaths['Province/State'] == state) & (df_deaths['Country/Region'] == 'US'), date_jh_format]):
+                df_deaths.loc[(df_deaths['Province/State'] == state) & (df_deaths['Country/Region'] == 'US'), date_jh_format] = deaths
+            else:
+                df_deaths = df_deaths.append(
+                    pd.Series([state, 'US', deaths], index=['Province/State', 'Country/Region', date_jh_format]),
+                    ignore_index=True,
+                )
+
+        # Fill nan with 0
+        self.df_cases = df_cases.fillna(0)
+        self.df_deaths = df_deaths.fillna(0)
 
         return self
 
@@ -168,7 +269,7 @@ class CovidDataAnalyzer:
             quality=('good', 'ok'),
     ):
         if not len(self.df_cases) or not len(self.df_deaths):
-            raise ValueError('Data has not been loaded properly. Call load_data() method first.')
+            raise ValueError('Data has not been loaded properly. Call load_data_jh() method first.')
 
         data = []   # type: [DataEstimate]
 
@@ -195,7 +296,7 @@ class CovidDataAnalyzer:
                 deaths_row = deaths_row.iloc[4:]  # Cut meta data at the beginning.
 
                 row_days = deaths_row.index.to_series()    # Split dates info a separate series.
-                row_days = row_days.apply(date_to_dofy)     # Convert dates into days of the year.
+                row_days = row_days.apply(jh_date_to_dofy)     # Convert dates into days of the year.
 
                 # Get real cases in the same format.
                 if row_province:
@@ -207,14 +308,18 @@ class CovidDataAnalyzer:
                             self.df_cases['Country/Region'] == row_country    # Sort by the same country and province.
                         ].iloc[0].iloc[4:]   # [1] gets the Series out of next() results, iloc[4:] cuts the unused meta data from the beginning.
 
-                # If we have less than 20 cases or 5 days of data or if the reported death rate is above 10%,
+                # If we have less than 20 cases or 7 days of data or if the reported death rate is above 10%,
                 # we cannot really estimate anything.
                 if (
-                        deaths_row.tail(1)[0] < 10
-                        or deaths_row.tail(5)[4] < 1
+                        deaths_row.tail(1)[0] < 20
+                        or deaths_row.tail(7)[4] < 1
                         or (deaths_row.sum() / cases.sum() * 100) > 10
                 ):
                     continue
+
+                population = 500000
+                if row_country == 'US' and row_province in self.states_population['state'].tolist():
+                    population = int(self.states_population.loc[self.states_population['state'] == row_province]['population'])
 
                 # Find the model fit.
                 fit = curve_fit(
@@ -227,7 +332,7 @@ class CovidDataAnalyzer:
                         (200, 1000, 3e7),
                     ),
                     check_finite=True,
-                    maxfev=100000
+                    maxfev=population,
                 )
                 fit_a = fit[0][0]
                 error_a = np.sqrt(fit[1][0][0])
@@ -236,12 +341,27 @@ class CovidDataAnalyzer:
                 fit_c = fit[0][2]
                 error_cases = np.sqrt(fit[1][2][2])
 
-                if error_a > 3 or error_peak_days > 150 or error_cases > 1e4:
+                # Make an estimated row with day # as values and dates as indexes.
+                cases_approximation = pd.Series(row_days, index=deaths_row.index)\
+                    .apply(
+                        lambda x: int(self.logistic_model((x+self.avg_days_to_death), fit_a, fit_peak_days, fit_c)/self.mortality_rate*100)
+                    )
+
+                # Just a quick estimate.
+                tail_cases_doubling_2d = float(deaths_row.tail(1))/self.mortality_rate*100*2**(self.avg_days_to_death/2)
+                tail_cases_doubling_4d = float(deaths_row.tail(1))/self.mortality_rate*100*2**(self.avg_days_to_death/4)
+                tail_cases_doubling_6d = float(deaths_row.tail(1))/self.mortality_rate*100*2**(self.avg_days_to_death/6)
+
+                # Estimated cases as of today.
+                tail_cases_approximation = int(cases_approximation.tail(1))
+
+                # If the estimation shows that there are more cases than an estimation with 2-day doubling rate, it's likely bad.
+                if error_a > 3 or error_peak_days > 150 or error_cases > population * 0.01 or tail_cases_approximation > tail_cases_doubling_2d:
                     estimate = 'bad'
                     # Skipp cases with not specified quality.
                     if 'bad' not in quality:
                         continue
-                elif error_a > 1 or error_peak_days > 50 or error_cases > 1e3:
+                elif error_a > 1 or error_peak_days > 50 or error_cases > population * 0.001:
                     # Skipp cases with not specified quality.
                     if 'ok' not in quality:
                         continue
@@ -251,12 +371,6 @@ class CovidDataAnalyzer:
                     if 'good' not in quality:
                         continue
                     estimate = 'good'
-
-                # Make an estimated row with day # as values and dates as indexes.
-                cases_approximation = pd.Series(row_days, index=deaths_row.index)\
-                    .apply(
-                        lambda x: int(self.logistic_model((x+self.avg_days_to_death), fit_a, fit_peak_days, fit_c)/self.mortality_rate*100)
-                    )
 
                 data.append(
                     DataEstimate(
@@ -277,15 +391,54 @@ class CovidDataAnalyzer:
                 continue
 
         # Sort data by fitment.
-        data = sorted(data, key=lambda x: x.error_cases)
+        data = sorted(data, key=lambda x: (x.estimate_order, x.error_cases))
 
         return data
 
 
-def render_page(data, filename):
+def save_graph(data):
+    """
+    Saves an png graph of provided data into a specified file.
+    :param data: DataEstimate object
+    :return:
+    """
+    # Cut blank data.
+    cases_approximation = data.cases_approximation.tail(len(data.cases_approximation) - data.cases_approximation.to_numpy().nonzero()[0][0])
+
+    cases_approximation = cases_approximation.copy()
+
+    filename = '{}.png'.format(data.name)
+    plot = cases_approximation.plot(
+        title='{} estimated vs. {} reported cases in {}{}'.format(
+            '{:,}'.format(data.current_cases_estimated),
+            '{:,}'.format(data.current_cases),
+            data.country,
+            ' - {}'.format(data.province) if data.province else '',
+        ),
+        kind='bar',
+        color='g',
+        legend=None,
+        use_index=False,
+        figsize=(19, 10),
+    )
+    figure = plot.get_figure()
+    figure.savefig(
+        os.path.join(
+            SITE_BASE_PATH,
+            filename,
+        ),
+        format='png',
+        bbox_inches='tight',
+        optimize=True
+    )
+    plt.close(figure)
+    return filename
+
+
+def render_page(data, filename, social_image_filename='social.jpg'):
     """
     Render a single HTML page with given data.
-    :param data: List of DataEstimate objects in
+    :param data: List of DataEstimate objects.
     :param filename: Filename to be generated, with extension and no path. e.g. index.html
     :return:
     """
@@ -298,16 +451,17 @@ def render_page(data, filename):
         site_url=SITE_URL,
         date_updated=datetime.now().strftime('%B %d'),
         debug=DEBUG,
+        filename=filename,
+        social_image_filename=social_image_filename,
     )
     with open(os.path.join(SITE_BASE_PATH, filename), 'w') as file:
         file.write(output)
 
 
 def render_pages():
-    """Render all site pages."""
-
-    covid_analyzer = CovidDataAnalyzer(base_path=DATA_BASE_PATH)
-    data = covid_analyzer.load_data(
+    """Render world pages from JH data."""
+    covid_analyzer = CovidDataAnalyzer(jh_base_path=JH_DATA_BASE_PATH, nyt_base_path=NYT_DATA_BASE_PATH)
+    data = covid_analyzer.load_data_jh(
         cases_file_path='csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv',
         deaths_file_path='csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv',
     ).approximate_data(
@@ -321,22 +475,45 @@ def render_pages():
     # Iterate over each region.
     for data_row in data:
 
-        # Render specific pages.
-        render_page([data_row], '{}'.format(data_row.page_name))
-
+        # NOTICE: Not being used at the moment as JH has not been updating the US data.
         # Save US related data.
         if data_row.country == 'US':
             data_us.append(data_row)
 
         # Save world related data.
         if data_row.country != 'US':
-            data_world.append(data_row)
+            # Render specific graphs and pages.
+            social_image_filename = save_graph(data_row)
+            render_page([data_row], '{}'.format(data_row.page_name), social_image_filename=social_image_filename)
 
-    # Render home page. (US)
-    render_page(data_us, 'index.html')
+            # Save world data for world.html
+            data_world.append(data_row)
 
     # Render the world page.
     render_page(data_world, 'world.html')
+
+    """Render US pages from NYT data."""
+    covid_analyzer = CovidDataAnalyzer(jh_base_path=JH_DATA_BASE_PATH, nyt_base_path=NYT_DATA_BASE_PATH)
+    data = covid_analyzer.load_data_nyt(states_file_path='us-states.csv').approximate_data(
+        include_countries=['All'],
+        exclude_countries=[],
+        quality=['good', 'ok', ]
+    )
+
+    data_us = []
+    # Iterate over each region.
+    for data_row in data:
+
+        # Save US related data.
+        if data_row.country == 'US':
+            # Render specific graphs and pages.
+            social_image_filename = save_graph(data_row)
+            render_page([data_row], '{}'.format(data_row.page_name), social_image_filename=social_image_filename)
+
+            data_us.append(data_row)
+
+    # Render home page. (US)
+    render_page(data_us, 'index.html')
 
 
 # Run page generator.
